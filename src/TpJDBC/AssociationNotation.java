0e255a778f2d16168d5183d1940220426deda44e
w14007405 <wasner83@gmail.com>
2015-10-21 10:56:45 +0200

    Q4

--- src/TpJDBC/AssociationNotation.java
+++ src/TpJDBC/AssociationNotation.java
@@ -3,6 +3,7 @@
  */
 package TpJDBC;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class AssociationNotation {
@@ -19,28 +20,57 @@ public class AssociationNotation {
         liens.add(lien);
     }
     public void supprimerLien (Module m, Etudiant e) {
-        Lien lien = new Lien(m,e);
-        lien
+        liens.remove(new Lien(m,e));
     }
     public void supprimerLien(Lien l) {
         liens.remove(l);
     }
     public Lien getLien(Module m, Etudiant e) {
-
+        Lien lien1 = new Lien(m,e);
+        for(Lien lien : liens){
+            if(lien1.equals(lien)){
+                return lien;
+            }
+        }
+        return null;
     }
     public Set<Lien> getLiens(Etudiant e) {
-
+        Set<Lien> lien2 = new HashSet<>();
+        for(Lien lien : liens){
+            if(lien.getEtudiant().equals(e)){
+                lien2.add(lien);
+            }
+        }
+        return lien2;
     }
     public Set<Lien> getLiens(Module m) {
-
+        Set<Lien> lien2 = new HashSet<>();
+        for(Lien lien : liens){
+            if(lien.getModule().equals(m)){
+                lien2.add(lien);
+            }
+        }
+        return lien2;
     }
     public Set<Module> getModules(Etudiant e) {
-
+        Set<Module> mod = new HashSet<>();
+        for(Lien lien : liens){
+            if(lien.getEtudiant().equals(e)){
+                mod.add(lien.getModule());
+            }
+        }
+        return mod;
     }
     public Set<Etudiant> getEtudiants(Module m) {
-
+        Set<Etudiant> etu = new HashSet<>();
+        for(Lien lien : liens){
+            if(lien.getModule().equals(m)){
+                etu.add(lien.getEtudiant());
+            }
+        }
+        return etu;
     }
     public AssociationNotation getInstance(){
-
+        return instance;
     }
 }
